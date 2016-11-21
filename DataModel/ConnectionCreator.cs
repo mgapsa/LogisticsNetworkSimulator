f80dd69cf3936acf2c52d96fc5c3a9b33255481d
mgapsa <gapsamikolaj@gmail.com>
2016-11-21 23:30:53 +0100

    Setup for connection creation

--- DataModel/ConnectionCreator.cs
+++ DataModel/ConnectionCreator.cs
@@ -8,6 +8,12 @@ namespace DataModel
 {
     class ConnectionCreator
     {
+        //ma byc w simulation modelu
+        //jak klikam na actorow ma sie to tu odolywac i sprawdzac i ustawiac
+        //exception handling zeby tam byl try catch i tam mi lapalo i tam bede wiedzial i wysiwetla jakich polaczen nie mozna robic
+        //right clikc nulluje mi actorow
+        //po dodanu actora wywoluje sprawdzenie czy mozna linie stworzyc - jak tak dodaje do listy connectionow connection 
+        //tworze connectionUI, rysuje, dodaje wartosc do HashMapy
         IActorUserControl ActorA;
         IActorUserControl ActorB;
 
---
+++ LogisticsNetworkSimulator/Actors/ConnectionUI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogisticsNetworkSimulator.Actors
+{
+    class ConnectionUI
+    {
+        //tutaj wszystko ziwazane z rysowanie, kontruktor przyjmujacy linie
+        //Linia
+        //wstepnie nie musze trzymac actorUI - bo jak cos to model ma odniesienie do modeli
+    }
+}
