c49922c1234f481421997e53e26aa874b1d441ba
mgapsa <gapsamikolaj@gmail.com>
2016-12-22 16:59:12 +0100

    Tests Added

---
+++ Tests/Database/BuyerTests.cs
@@ -0,0 +1,39 @@
+﻿using DataModel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Database
+{
+    [TestFixture]
+    class BuyerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //before each every method it will be executed
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            //at the end
+        }
+
+        [Test]
+        public void DefaultCtorTest()
+        {
+        //    var b = new Buyer();
+        //    Assert.AreEqual(0, b.Id);
+        //    Assert.AreEqual(0.0, p.Price);
+        //    Assert.AreEqual(null, p.Name);
+        //    Assert.AreEqual(null, p.Description);
+        //    Assert.AreEqual(null, p.Img);
+        //    Assert.AreEqual(0, p.CategoryId);
+        //    Assert.AreEqual(0, p.Number);
+        }
+    }
+}
---
+++ Tests/Model/BuyerTests.cs
@@ -0,0 +1,44 @@
+﻿using DataModel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Model
+{
+    [TestFixture]
+    class BuyerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            //before each every method it will be executed
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            //at the end
+        }
+
+        [Test]
+        public void DefaultCtorTest()
+        {
+            var b = new Buyer();
+            Assert.AreEqual(0, b.Id);
+            Assert.AreEqual(b.OptionA, EnumTypes.BuyerAOptions.Static);
+            Assert.AreEqual(b.Amount, 50);
+            Assert.AreEqual(b.MinAmount, 25);
+            Assert.AreEqual(b.MaxAmount, 75);
+            Assert.AreEqual(b.Lambda, 50);
+            Assert.AreEqual(b.MeanOptionA, 50);
+            Assert.AreEqual(b.DeviationOptionA, 5);
+            Assert.AreEqual(b.OptionB, EnumTypes.BuyerBOptions.Static);
+            Assert.AreEqual(b.Minutes, 20);
+            Assert.AreEqual(b.MeanOptionB, 50);
+            Assert.AreEqual(b.DeviationOptionB, 10);
+        }
+    }
+}
